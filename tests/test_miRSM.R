a1ff96ab9f1ee770d6637fca6ba3311dea0060c1
Junpeng Zhang <zhangjunpeng_411@yahoo.com>
2018-08-04 15:28:45 +0800

    Add files via upload

--- tests/test_miRSM.R
+++ tests/test_miRSM.R
@@ -2,22 +2,15 @@ library(testthat)
 library(miRSM)
 
 # Load datasets
-miRExpcsv <- system.file("extdata","miRExp.csv",package="miRSM")
-miRExp <- read.csv(miRExpcsv, header=FALSE, sep=",")
-miRExp <- data_tidy(miRExp)
-ceRExpcsv <- system.file("extdata","ceRExp.csv",package="miRSM")
-ceRExp <- read.csv(ceRExpcsv, header=FALSE, sep=",")
-ceRExp <- data_tidy(ceRExp)
-mRExpcsv <- system.file("extdata","mRExp.csv",package="miRSM")
-mRExp <- read.csv(mRExpcsv, header=FALSE, sep=",")
-mRExp <- data_tidy(mRExp)
-miRTargetcsv <- system.file("extdata","miRTarget.csv",package="miRSM")
-miRTarget <- read.csv(miRTargetcsv, header=FALSE, sep=",")
+data(ceRExp)
+data(mRExp)
+data(miRExp)
+data(miRTarget)
 
 # Identify gene co-expression modules using WGCNA method
 modulegenes_WGCNA <- module_WGCNA(ceRExp, mRExp)
 
 
 test_that("Test miRSM", {
-    expect_equal(module_WGCNA(ceRExp, mRExp), modulegenes_WGCNA)    
+    expect_equal(module_WGCNA(ceRExp, mRExp), modulegenes_WGCNA)
 })
